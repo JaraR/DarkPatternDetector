6210a57db7d040e5586764d0b7b2fe7c081639ab
JaraR <jara_r1993@yahoo.com>
2024-10-08 16:00:11 +0100

    Updated initialApp.js to current AboutUs.js without previous style choices.

--- frontend/src/pages/AboutUs.js
+++ frontend/src/pages/AboutUs.js
@@ -3,8 +3,27 @@ import React from "react";
 const About = () => {
   return (
     <div>
-      <h2>About Page</h2>
+      <div>
+        <h2>What does this detection tool do?</h2>
+        <p>This tool detects dark patterns on X/Twitter.</p>
+      </div>
+      <div>
+        <h2>What are dark patterns?</h2>
+        <p className="font-change">"Dark patterns are tricks used in websites and apps that make you do things that you didn't mean to, 
+          like buying or signing up for something." - <a href="https://www.deceptive.design">https://www.deceptive.design</a></p>
+      </div>
+      <div>
+        <h2>What Dark patterns does this tool detect?</h2>
+        <p>Emotional Steering</p>
+        <p>Infinite Scrolling</p>
+        <p>Autoplay Videos</p>
+        <p>Privacy Zuckering</p>
+        <p>Engagement Notification</p>
+        <p>Obstructing</p>
+        <p>Promoted Tweets and Ads that Blend In</p>
+      </div>
     </div>
+    
   );
 };
 
