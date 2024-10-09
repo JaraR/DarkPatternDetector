67033fd6df5c0d1381d55fe47842ddd384f4c236
Jara <jara_r1993@yahoo.com>
2024-10-09 16:17:34 +0100

    Added Material UI React component library to the project to speed up development time and tested a component to check functionality.

--- frontend/src/pages/Results.js
+++ frontend/src/pages/Results.js
@@ -1,4 +1,10 @@
 import React from "react";
+import Accordion from '@mui/material/Accordion';
+import AccordionSummary from '@mui/material/AccordionSummary';
+import AccordionDetails from '@mui/material/AccordionDetails';
+import Typography from '@mui/material/Typography';
+// import ArrowDownwardIcon from '@mui/icons-material/ArrowDownward';
+// import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
 
 const Results = () => {
   return (
@@ -6,6 +12,38 @@ const Results = () => {
       <div>
         <p>Placeholder for Total # DP detected</p>
         <p>Dark Patterns Detected</p>
+      </div>
+      <div>
+      <Accordion>
+        <AccordionSummary
+          // expandIcon={<ArrowDownwardIcon />}
+          // aria-controls="panel1-content"
+          // id="panel1-header"
+        >
+          <Typography>Emotional Steering</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+            malesuada lacus ex, sit amet blandit leo lobortis eget.
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+      <Accordion>
+        <AccordionSummary
+          // expandIcon={<ArrowDropDownIcon />}
+          // aria-controls="panel2-content"
+          // id="panel2-header"
+        >
+          <Typography>Infinite Scrolling</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+            malesuada lacus ex, sit amet blandit leo lobortis eget.
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
       </div>
       <div>
         <p>Emotional Steering</p>
