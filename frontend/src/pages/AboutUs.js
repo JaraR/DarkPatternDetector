9f2af2996747d39a7d19b60c0c0546e2d90b5b13
Jara <jara_r1993@yahoo.com>
2024-10-09 21:57:52 +0100

    Basic accordion component created for each Dark Pattern.

--- frontend/src/pages/AboutUs.js
+++ frontend/src/pages/AboutUs.js
@@ -1,4 +1,10 @@
 import React from "react";
+import Accordion from '@mui/material/Accordion';
+import AccordionSummary from '@mui/material/AccordionSummary';
+import AccordionDetails from '@mui/material/AccordionDetails';
+import Typography from '@mui/material/Typography';
+// import ArrowDownwardIcon from '@mui/icons-material/ArrowDownward';
+// import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
 
 const About = () => {
   return (
@@ -14,13 +20,111 @@ const About = () => {
       </div>
       <div>
         <h2>What Dark patterns does this tool detect?</h2>
-        <p>Emotional Steering</p>
-        <p>Infinite Scrolling</p>
-        <p>Autoplay Videos</p>
-        <p>Privacy Zuckering</p>
-        <p>Engagement Notification</p>
-        <p>Obstructing</p>
-        <p>Promoted Tweets and Ads that Blend In</p>
+        <Accordion>
+        <AccordionSummary
+          // expandIcon={<ArrowDownwardIcon />}
+          // aria-controls="panel1-content"
+          // id="panel1-header"
+        >
+          <Typography>Emotional Steering</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+            malesuada lacus ex, sit amet blandit leo lobortis eget.
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+      <Accordion>
+        <AccordionSummary
+          // expandIcon={<ArrowDropDownIcon />}
+          // aria-controls="panel2-content"
+          // id="panel2-header"
+        >
+          <Typography>Infinite Scrolling</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+            malesuada lacus ex, sit amet blandit leo lobortis eget.
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+      <Accordion>
+        <AccordionSummary
+          // expandIcon={<ArrowDownwardIcon />}
+          // aria-controls="panel1-content"
+          // id="panel1-header"
+        >
+          <Typography>Autoplay Videos</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+            malesuada lacus ex, sit amet blandit leo lobortis eget.
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+      <Accordion>
+        <AccordionSummary
+          // expandIcon={<ArrowDownwardIcon />}
+          // aria-controls="panel1-content"
+          // id="panel1-header"
+        >
+          <Typography>Privacy Zuckering</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+            malesuada lacus ex, sit amet blandit leo lobortis eget.
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+      <Accordion>
+        <AccordionSummary
+          // expandIcon={<ArrowDownwardIcon />}
+          // aria-controls="panel1-content"
+          // id="panel1-header"
+        >
+          <Typography>Engagement Notification</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+            malesuada lacus ex, sit amet blandit leo lobortis eget.
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+      <Accordion>
+        <AccordionSummary
+          // expandIcon={<ArrowDownwardIcon />}
+          // aria-controls="panel1-content"
+          // id="panel1-header"
+        >
+          <Typography>Obstructing</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+            malesuada lacus ex, sit amet blandit leo lobortis eget.
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+      <Accordion>
+        <AccordionSummary
+          // expandIcon={<ArrowDownwardIcon />}
+          // aria-controls="panel1-content"
+          // id="panel1-header"
+        >
+          <Typography>Promoted Tweets and Ads that Blend In</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+            malesuada lacus ex, sit amet blandit leo lobortis eget.
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
       </div>
     </div>
     
