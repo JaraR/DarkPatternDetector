67e7e8d888f79cbc1ba18b3ef8be0f7b7e303470
LingJiang9 <D22128264@mytudublin.ie>
2024-10-27 18:21:26 +0000

    Added update piechart on content-script

--- frontend/public/background.js
+++ frontend/public/background.js
@@ -8,3 +8,23 @@ function updateBadge(count) {
   chrome.action.setBadgeText({ text: count.toString() });
   chrome.action.setBadgeBackgroundColor({ color: "#FF0000" });
 }
+function updateResults(count) {
+  autoplayCount = count;
+  console.log("Autoplay count updated in background:", autoplayCount);
+  chrome.storage.local.set({ autoplayCount });
+}
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+  //--- badge count---
+  if (message.type === "updateBadge") {
+    updateBadge(message.count);
+  }
+
+  // ---Results.js---
+  if (message.type === "updateAutoplay") {
+    updateResults(message.count);
+    sendResponse({ count: autoplayCount });
+  }
+  if (message.type === "getResults") {
+    sendResponse({ count: autoplayCount });
+  }
+});
--- frontend/public/content-scripts/autoplay.js
+++ frontend/public/content-scripts/autoplay.js
@@ -13,6 +13,20 @@ const updateBadgeCount = () => {
     });
   });
 };
+//this could be simipfied later with a single function in update badge
+function updateAutoplayCount() {
+  chrome.storage.local.get(["autoplayCount"], function (result) {
+    let currentCount = result.autoplayCount || 0;
+    currentCount += 1;
+    chrome.storage.local.set({ autoplayCount: currentCount });
+
+    // Notify the background script to update the autoplay count
+    chrome.runtime.sendMessage({
+      type: "updateAutoplay",
+      count: currentCount,
+    });
+  });
+}
 function checkAutoplay() {
   const videos = document.querySelectorAll("video");
   videos.forEach((video) => {
@@ -23,14 +37,13 @@ function checkAutoplay() {
 
         alertedVideos.push(video);
         updateBadgeCount();
+        updateAutoplayCount();
       }
     }
   });
 }
 
-const observer = new MutationObserver(() => {
-  checkAutoplay();
-});
+const observer = new MutationObserver(checkAutoplay);
 
 observer.observe(document.body, {
   childList: true,
