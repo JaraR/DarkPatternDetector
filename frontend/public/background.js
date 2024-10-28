5725a11a358f821ef56182ecef19fcf8c419c986
LingJiang9 <D22128264@mytudublin.ie>
2024-10-28 14:08:05 +0000

    Fix bug: Async data

--- frontend/public/background.js
+++ frontend/public/background.js
@@ -13,18 +13,30 @@ function updateResults(count) {
   console.log("Autoplay count updated in background:", autoplayCount);
   chrome.storage.local.set({ autoplayCount });
 }
-chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-  //--- badge count---
-  if (message.type === "updateBadge") {
-    updateBadge(message.count);
-  }
+function handleMessage(message, sendResponse) {
+  switch (message.type) {
+    case "updateBadge":
+      updateBadge(message.count);
+      break;
 
-  // ---Results.js---
-  if (message.type === "updateAutoplay") {
-    updateResults(message.count);
-    sendResponse({ count: autoplayCount });
+    case "updateAutoplay":
+      updateResults(message.count, sendResponse);
+      break;
+
+    case "getResults":
+      chrome.storage.local.get(["autoplayCount"], (result) => {
+        const storedAutoplayCount = result.autoplayCount || 0;
+        sendResponse({ count: storedAutoplayCount });
+      });
+      break;
+
+    default:
+      console.warn("Unknown message type:", message.type);
+      break;
   }
-  if (message.type === "getResults") {
-    sendResponse({ count: autoplayCount });
-  }
+}
+
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+  handleMessage(message, sendResponse);
+  return true;
 });
