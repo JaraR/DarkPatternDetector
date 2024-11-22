4a76cbbdd93aea13f99d43449d88d35ee528dc54
Jara <jara_r1993@yahoo.com>
2024-11-22 08:21:02 +0000

    Initial commit. Added Tip Carousel on home/results page.
    Tip Carousel will display tips regarding privacy settings, what they do, and information they can give away. Carousel has autoplay feature to rotate between different tips.

--- frontend/src/pages/home.jsx
+++ frontend/src/pages/home.jsx
@@ -7,6 +7,7 @@ import AboutUsTab from "@/components/ui/AboutUsTab";
 import BottomNavigation from "@/components/ui/BottomNavigation";
 import Typography from "@mui/material/Typography";
 import SettingTab from "@/components/ui/SettingTab";
+import { TipCarousel } from "@/components/TipCarousel";
 
 export function Home() {
   const [autoplayCount, setAutoplayCount] = useState(0);
@@ -62,6 +63,7 @@ export function Home() {
               autoplayCount={autoplayCount}
               promotedAdsCount={promotedAdsCount}
             />
+            <TipCarousel />
             <BottomNavigation />
           </div>
         </TabsContent>
