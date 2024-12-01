1d821fa2636b09e6aaedca5d14f5d5584b3ccadc
LingJiang9 <D22128264@mytudublin.ie>
2024-12-01 23:38:08 +0000

    Overlay feature added for promoteds ads

--- frontend/public/content-scripts/promoted-ads.js
+++ frontend/public/content-scripts/promoted-ads.js
@@ -19,10 +19,60 @@ function updateStorageAndNotify(type, storageKey, messageType) {
 // Function to highlight the ad feed
 function highlightAd(adSpan) {
   adSpan.style.borderLeft = "16px dashed #bd60ff";
-
   adSpan.style.backgroundColor = "#fff4e0";
-
   adSpan.style.paddingLeft = "8px";
+}
+
+function addAdOverlay(article) {
+  const contentWrapper = document.createElement("div");
+  while (article.firstChild) {
+    contentWrapper.appendChild(article.firstChild);
+  }
+  contentWrapper.style.filter = "blur(5px)";
+  contentWrapper.style.position = "relative";
+  article.appendChild(contentWrapper);
+  const overlay = document.createElement("div");
+  overlay.style.position = "absolute";
+  overlay.style.top = "0";
+  overlay.style.left = "0";
+  overlay.style.width = "100%";
+  overlay.style.height = "100%";
+  overlay.style.display = "flex";
+  overlay.style.flexDirection = "column";
+  overlay.style.justifyContent = "center";
+  overlay.style.alignItems = "center";
+  overlay.style.zIndex = "9999";
+  const icon = document.createElement("img");
+  icon.src = "https://i.postimg.cc/dtYtfDGx/ads-action.png";
+
+  icon.style.width = "80px";
+  icon.style.height = "80px";
+  icon.alt = "Promoted Ads";
+  icon.style.zIndex = "9999";
+  // Append the icon to the overlay
+  overlay.appendChild(icon);
+  // Append the overlay to the article
+  article.appendChild(overlay);
+  const text = document.createElement("span");
+  text.textContent = "Promoted Ads Detected";
+  text.style.marginTop = "10px";
+  text.style.fontSize = "16px";
+  text.style.fontWeight = "bold";
+  text.style.color = "#c38ee8";
+
+  overlay.appendChild(text);
+
+  article.appendChild(overlay);
+  //hover effect
+  article.addEventListener("mouseenter", () => {
+    contentWrapper.style.filter = "none";
+    overlay.style.display = "none";
+  });
+
+  article.addEventListener("mouseleave", () => {
+    contentWrapper.style.filter = "blur(5px)";
+    overlay.style.display = "flex";
+  });
 }
 
 // Function to check for ads
@@ -34,42 +84,15 @@ function checkForAds() {
       alertedAds.push(span);
       // alert("Promoted ads detected!");
       console.log("Promoted ad detected:", span);
-
       highlightAd(span);
 
       const article = span.closest("article");
+
       if (article) {
-        const tweetTextDiv = article.querySelector('[data-testid="tweetText"]');
-
-        // Apply the blur effect to the tweetTextDiv
-        if (tweetTextDiv) {
-          tweetTextDiv.style.filter = "blur(5px)";
-          tweetTextDiv.addEventListener("mouseenter", () => {
-            tweetTextDiv.style.filter = "none";
-          });
-
-          tweetTextDiv.addEventListener("mouseleave", () => {
-            tweetTextDiv.style.filter = "blur(5px)";
-          });
-        }
-
-        // const videoDiv = document.querySelector('[data-testid="videoPlayer"]');
-        // if (videoDiv) {
-        //   videoDiv.style.filter = "blur(5px)";
-        // }
+        addAdOverlay(article);
 
         const articleLinks = article.querySelectorAll("a");
-
         articleLinks.forEach((link) => {
-          link.style.setProperty("filter", "blur(5px)", "important");
-          link.addEventListener("mouseenter", () => {
-            link.style.setProperty("filter", "none", "important");
-          });
-
-          link.addEventListener("mouseleave", () => {
-            link.style.setProperty("filter", "blur(5px)", "important");
-          });
-
           if (
             link.hasAttribute("target") &&
             link.getAttribute("target") === "_blank"
